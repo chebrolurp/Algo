d90685abecc7c8c5e9d58b7f1d888ca58af334bb
chebrolurp <ramprassanthchebrolu993@gmail.com>
2022-02-10 17:00:11 +0530

    graph search added

---
+++ bfs.py
@@ -0,0 +1,39 @@
+# defaultdict does not give key error very useful to represent a graph
+from collections import defaultdict
+
+# input: edges and  output: dictionary(adjacency list)
+def Graph(edges):
+    graph = defaultdict(list)
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])
+    return graph
+
+def bfs(visited,graph, node):
+    
+    queue = []
+    
+    # starting node
+    queue.append(node)
+    visited.append(node)
+    
+    while queue:
+
+        s = queue.pop(0)
+        print (s, end = " ") 
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+                
+nodes = ['a','b','c','d','e','f','g','h','i','j']
+edges = [('a','b'),('b','c'),('c','a'),('c','d'),('d','f'),('e','f'),('f','i'),('i','a'),('d','b'),
+         ('c','j'),('c','e'),('g','h'),('h','e'),('g','j'),('f','g')]
+
+graph = Graph(edges)
+
+visited = []
+print('BFS')
+bfs(visited,graph, 'i')
+print()
---
+++ dfs.py
@@ -0,0 +1,33 @@
+# defaultdict does not give key error very useful to represent a graph
+from collections import defaultdict
+
+# input: edges and  output: dictionary(adjacency list)
+def Graph(edges):
+    graph = defaultdict(list)
+    for edge in edges:
+        graph[edge[0]].append(edge[1])
+        graph[edge[1]].append(edge[0])
+    return graph
+
+# Depth first search
+def dfs(visited, graph, node):
+    
+    if node not in visited:
+        
+        print(node, end = " ")
+        visited.append(node)
+        
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+            
+            
+nodes = ['a','b','c','d','e','f','g','h','i','j']
+edges = [('a','b'),('b','c'),('c','a'),('c','d'),('d','f'),('e','f'),('f','i'),('i','a'),('d','b'),
+         ('c','j'),('c','e'),('g','h'),('h','e'),('g','j'),('f','g')]
+
+graph = Graph(edges)
+
+visited = []
+print('DFS')
+dfs(visited,graph,'i')
+print()
